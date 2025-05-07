255c3ad23b9d513eba5e3b71de20ff314c56b618
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-07 11:19:58 +0200

    Add Identity and EF Core packages; update gRPC service
    Updated `Presentation.csproj` to include new package references for ASP.NET Core Identity and Entity Framework Core, including `Microsoft.AspNetCore.Identity`, `Microsoft.AspNetCore.Identity.EntityFrameworkCore`, `Microsoft.EntityFrameworkCore.Design`, `Microsoft.EntityFrameworkCore.SqlServer`, and `Microsoft.EntityFrameworkCore.Tools`.
    
    Modified `Program.cs` to replace `GreeterService` with `AccountService` in the gRPC service mapping, while adjusting the order of operations and retaining the root endpoint message.

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,14 +1,11 @@
 using Presentation.Services;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
 builder.Services.AddGrpc();
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+
+var app = builder.Build();
+app.MapGrpcService<AccountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
