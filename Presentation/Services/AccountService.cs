bd560431e54c59941eae1a2868b4829b9a32f57f
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-07 11:26:13 +0200

    CreateAccount method created

--- Presentation/Services/AccountService.cs
+++ Presentation/Services/AccountService.cs
@@ -1,9 +1,34 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Grpc.Core;
+using Microsoft.AspNetCore.Identity;
 
 namespace Presentation.Services;
 
-public class AccountService(UserManager<IdentityUser> userManager)
+public class AccountService(UserManager<IdentityUser> userManager) : AccountGrpcService.AccountGrpcServiceBase
 {
     private readonly UserManager<IdentityUser> _userManager = userManager;
+
+    public override async Task<CreateAccountReply> CreateAccount(CreateAccountRequest request, ServerCallContext context)
+    {
+        var user = new IdentityUser
+        {
+            UserName = request.Email,
+            Email = request.Email,
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        var reply = new CreateAccountReply
+        {
+            Succeeded = result.Succeeded,
+            Message = result.Succeeded ? "Account was created successfully." : string.Join(", ", result.Errors.Select(e => e.Description))
+        };
+
+        if (result.Succeeded)
+        {
+            reply.UserId = user.Id;
+        }
+
+        return reply;
+    }
 
 }
