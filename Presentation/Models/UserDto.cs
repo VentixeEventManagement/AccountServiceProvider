2a237097fdd6a3a95152e129bfcfc06aec99e16d
Björn Åhström <bjorn.ahstrom@icloud.com>
2025-05-19 17:53:30 +0200

    Add role management to account services and models
    - Updated `Account` message in `account.proto` to include `roleName`.
    - Modified `AccountService` to accept `RoleManager<IdentityRole>`.
    - Enhanced `CreateAccount` to manage user roles and handle errors.
    - Updated `GetAccounts` and `GetAccount` methods to include user roles.
    - Introduced `UserDto` class for user data with role information.

---
+++ Presentation/Models/UserDto.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Presentation.Models;
+
+public class UserDto : IdentityUser
+{
+    public string UserId { get; set; } = null!;
+    public string Role {  get; set; } = null!;
+}
